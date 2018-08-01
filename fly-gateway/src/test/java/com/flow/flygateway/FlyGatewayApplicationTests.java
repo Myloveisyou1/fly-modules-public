f491f210f73da3c204fed909e132c3ac3124fe81
liuyingjie <123456>
2018-08-01 17:26:06 +0800

    Initial commit

---
+++ fly-gateway/src/test/java/com/flow/flygateway/FlyGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.flow.flygateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlyGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
